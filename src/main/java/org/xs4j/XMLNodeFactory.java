8cdcfef2ba86ba2a17ab0a2b1d9d2818ba94bb9d
Turski Marek <marek.turski.ext@nokia.com>
2016-12-11 15:24:56 +0100

    node factory exposing methods as well

--- src/main/java/org/xs4j/XMLNodeFactory.java
+++ src/main/java/org/xs4j/XMLNodeFactory.java
@@ -6,7 +6,7 @@ import java.util.Map;
 /**
  * Created by mturski on 11/8/2016.
  */
-public class XMLNodeFactory {
+public final class XMLNodeFactory {
     static final String QNAME_SEPARATOR = ":";
     static final String XMLNS_WITH_SEPARATOR = XMLConstants.XMLNS_ATTRIBUTE + QNAME_SEPARATOR;
 
@@ -23,11 +23,11 @@ public class XMLNodeFactory {
     }
 
 
-    XMLNode createNode(long id, String localName, Map<String, String> attributeByName) {
+    public XMLNode createNode(long id, String localName, Map<String, String> attributeByName) {
         return new XMLNodeImpl(id, null, null, localName, attributeByName);
     }
 
-    XMLNode createNode(long id, String namespace, String prefix, String localName, Map<String, String> attributeByName) {
+    public XMLNode createNode(long id, String namespace, String prefix, String localName, Map<String, String> attributeByName) {
         return new XMLNodeImpl(id, namespace, prefix, localName, attributeByName);
     }
 }
